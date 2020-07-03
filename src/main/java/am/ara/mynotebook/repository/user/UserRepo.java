fa6a9d09a27a1f1ef9d2f5514145b84738b483a2
Ara-Ellaryan <araell@mail.ru>
2020-07-03 08:51:51 +0400

    adding @Repository annotation in userRepo

--- src/main/java/am/ara/mynotebook/repository/user/UserRepo.java
+++ src/main/java/am/ara/mynotebook/repository/user/UserRepo.java
@@ -4,6 +4,7 @@ import am.ara.mynotebook.entity.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepo extends JpaRepository<UserEntity, Integer> {
 
 }
